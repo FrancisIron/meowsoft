a83c368772bd7982e31f45f0b9ae2da71fc31df6
Giaccomo A. Darigo Herrera <g.darigo01@ufromail.cl>
2019-02-25 21:40:33 -0300

    Update scripts.js

--- dev/test-a/js/scripts.js
+++ dev/test-a/js/scripts.js
@@ -1,1 +1,16 @@
 // JavaScript source code
+var mobile = false;
+
+$( document ).ready(function() {
+  checkMobile();
+  
+  if (mobile) {
+    $("#test").text("This is a mobile device");
+  }
+}
+                    
+function checkMobile(){
+  if (/Mobi|Android/i.test(navigator.userAgent)) {
+    mobile = true; // mobile!
+  }
+}
