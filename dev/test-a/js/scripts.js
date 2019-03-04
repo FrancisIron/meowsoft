3080f8e1fa0cc8b65c4213a7fbac36b721987e60
Giaccomo Darigo <gadh210@gmail.com>
2019-03-04 16:40:19 -0300

    Sidenav fix

--- dev/test-a/js/scripts.js
+++ dev/test-a/js/scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 				$(icon).html('arrow_drop_up');
 				$(icon).fadeIn(300);
 			});
-		}, onCloseStart: function {
+		}, onCloseStart: function() {
 			var icon = $(this).find('.collapsible-arrow-icon');
 			$(icon).fadeOut(300, function() {
 				$(icon).html('arrow_drop_down');
