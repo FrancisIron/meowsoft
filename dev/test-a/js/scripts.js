b4e2b3b609db75f557078b5fe60f39ed6778091b
Giaccomo Darigo <gadh210@gmail.com>
2019-03-06 01:30:30 -0300

    Firestore background image fix

--- dev/test-a/js/scripts.js
+++ dev/test-a/js/scripts.js
@@ -73,11 +73,16 @@ $(document).ready(function() {
 		dots: true,
 		fade: true
 	});
+	$('.slick-slider-backgrounds').addClass('temp-reference-class');
 	$('.slick-slider-backgrounds').slick('slickGoTo',2);
 	$('.slick-track').addClass('not-so-random-useless-width');
 	$('.slick-slider-backgrounds').on('afterChange', function(slick, currentSlide){
+		if ($('.slick-slider-backgrounds.temp-reference-class').length > -1) {
+			$('.slick-slider-backgrounds').removeClass('temp-reference-class');
+		} else {
+			fnSaveUserSettingsBackgroundImage(bgN);
+		}
 		var bgN = "bg" + ($(this).slick('slickCurrentSlide') + 1);
-		fnSaveUserSettingsBackgroundImage(bgN);
 		changeBackgroundImage(bgN);
 	});
 
