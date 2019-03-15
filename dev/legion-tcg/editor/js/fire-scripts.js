cc20e8d8e369cedf408fd31df2ed9541ed79e6f6
Giaccomo Darigo <gadh210@gmail.com>
2019-03-15 20:01:06 -0300

    test

--- dev/legion-tcg/editor/js/fire-scripts.js
+++ dev/legion-tcg/editor/js/fire-scripts.js
@@ -12,8 +12,7 @@ $(document).ready(function () {
         //signInSuccessUrl: 'https://meowsoft.net/dev/legion-tcg/editor/',
         signInOptions: [
             firebase.auth.GoogleAuthProvider.PROVIDER_ID
-        ], signInSuccess: false,
-        signInSuccessUrl: '/dev/legion-tcg/editor/'
+        ], signInSuccessUrl: '/dev/legion-tcg/editor/'
     };
     // Initialize the FirebaseUI Widget using Firebase.
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
