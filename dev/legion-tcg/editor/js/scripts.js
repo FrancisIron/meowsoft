fe2e76ff2d4ec8621a1d2ce9e5d24ef1b0d71828
Giaccomo Darigo <gadh210@gmail.com>
2019-03-15 17:09:37 -0300

    css update

--- dev/legion-tcg/editor/js/scripts.js
+++ dev/legion-tcg/editor/js/scripts.js
@@ -20,34 +20,11 @@ $(document).ready(function () {
     /** Button Scripts **/
     $('#btn-card-new').click(function () {
         console.log('DEBUG: btn-card-new pressed');
-        $('input, textarea').val('');
-        $('input + label, textarea + label').removeClass('active');
+        clearAllInputs();
     });
     $('#btn-card-save').click(function () {
         console.log('DEBUG: btn-card-save pressed');
-        if (!($('#card-id').val())) {
-            console.log('DEBUG: card-id is empty');
-            fnUpdateCID();
-            $('#card-id').val(cid);
-            $('#card-id').addClass('active');
-        }
-        var card = {
-            id: $('#card-id').val(),
-            name: $('#card-name').val(),
-            descriptionEN: $('#card-descriptionEN').val(),
-            descriptionES: $('#card-descriptionES').val(),
-            rarity: $('#card-rarity').val(),
-            type: $('#card-type').val(),
-            faction: $('#card-faction').val(),
-            value: $('#card-value').val(),
-            damage: $('#card-damage').val(),
-            defense: $('#card-defense').val(),
-            health: $('#card-health').val()
-        };
-        if (!card['descriptionEN']) card['descriptionEN'] = "";
-        if (!card['descriptionES']) card['descriptionES'] = "";
-        console.log('DEBUG: card data:',card);
-        fnUpdateCard(card);
+        saveCardBtn();
     });
     $('#btn-card-delete').click(function () {
         console.log('DEBUG: btn-card-delete pressed');
@@ -55,9 +32,42 @@ $(document).ready(function () {
     });
     /** Button Scripts End **/
     /** Load Ready **/
-    $('input, textarea').disableAutoFill();
+    clearAllInputs();
     onLoad();
 });
+
+function clearAllInputs() {
+    console.log('DEBUG: clearing inputs');
+    $('input, textarea').disableAutoFill();
+    $('input, textarea').val('');
+    $('input + label, textarea + label').removeClass('active');
+}
+
+function saveCardBtn() {
+    if (!($('#card-id').val())) {
+        console.log('DEBUG: card-id is empty');
+        fnUpdateCID();
+        $('#card-id').val(cid);
+        $('#card-id').addClass('active');
+    }
+    var card = {
+        id: $('#card-id').val(),
+        name: $('#card-name').val(),
+        descriptionEN: $('#card-descriptionEN').val(),
+        descriptionES: $('#card-descriptionES').val(),
+        rarity: $('#card-rarity').val(),
+        type: $('#card-type').val(),
+        faction: $('#card-faction').val(),
+        value: $('#card-value').val(),
+        damage: $('#card-damage').val(),
+        defense: $('#card-defense').val(),
+        health: $('#card-health').val()
+    };
+    if (!card['descriptionEN']) card['descriptionEN'] = "";
+    if (!card['descriptionES']) card['descriptionES'] = "";
+    console.log('DEBUG: card data:', card);
+    fnUpdateCard(card);
+}
 
 function onLoad() {
     // Destroy preloader
@@ -67,30 +77,3 @@ function onLoad() {
     });
 }
 
-function onSubmit1() {
-    if ($("#droplistRegion option:selected").val() == "") {
-        M.toast({ html: 'Debe seleccionar una Region', classes: 'rounded' });
-    } else if ($("#droplistInstitucion option:selected").val() == "") {
-        M.toast({ html: 'Debe seleccionar una Institucion', classes: 'rounded' });
-    } else if ($("#droplistSede option:selected").val() == "") {
-        M.toast({ html: 'Debe seleccionar una Sede', classes: 'rounded' });
-    } else if ($("#id-laboratorio").val() == "") {
-        M.toast({ html: 'Debe ingresar un Id de Laboratorio', classes: 'rounded' });
-    } else if ($("#id-tecnico").val() == "") {
-        M.toast({ html: 'Debe ingresar un Id de Tecnico', classes: 'rounded' });
-    } else {
-        // Do Something Here...
-        $("#preloader").show();
-        $("#div-inputs").fadeOut(1000, function () {
-            $("#div-inputs").hide();
-            $("#preloader").css({ 'background-color': '' });
-
-            var Data = require('./data.js');
-            $("#output-p").text(Data.getSystem);
-
-            $("#div-output").fadeIn(100, function () {
-                $("#preloader").hide();
-            });
-        });
-    }
-}
