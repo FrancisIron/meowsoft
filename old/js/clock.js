a6c5dff4230eedb5684f817c6cc166349c4cd022
Giaccomo DH <gadh210@gmail.com>
2022-10-16 15:17:42 -0300

    New front page

--- old/js/clock.js
+++
@@ -1,8 +0,0 @@
-function updateClock(){
-	let time = new Date();
-	let hours = time.getHours();
-	hours = (hours < 10 ? "0" : "") + hours;
-	let minutes = time.getMinutes();
-	minutes = (minutes < 10 ? "0" : "") + minutes;
-	document.getElementById("clock").firstChild.nodeValue = hours + ":" + minutes;
-}
