4d069e6e34598bc2ab1155fc38f569e17d1c291b
Giaccomo DH <gadh210@gmail.com>
2022-10-21 17:44:37 -0300

    updated anim-active-edge

--- resources/js/anim.js
+++ resources/js/anim.js
@@ -1,11 +1,34 @@
 $(document).ready(function(){
+    /** Ripple */
+    // $("body").on("click", ".anim-active-ripple", function(e) {
+    $(".anim-active-ripple").parent().click(function(e){
+        var posX = e.pageX - $(this).offset().left;
+        var posY = e.pageY - $(this).offset().top;
+        var ripple = $("<span class='anim-ripple' style='top:"+posY+";left:"+posX+";'></span>").appendTo($(this));    
+        setTimeout(function() {ripple.remove();}, 1000);
+    });
+    /** Edge **/
+    $(".anim-active-edge").parent().click(function(e){
+        var posX = e.pageX - $(this).offset().left;
+        var parentWidth = $(this).outerWidth();
+        var edge_l = $("<span class='anim-edge-left' style='left:"+posX+";'></span>").appendTo($(this));
+        var edge_r = $("<span class='anim-edge-right' style='left:"+posX+";'></span>").appendTo($(this));
+        edge_l.animate({
+            left: posX - parentWidth
+        }, 600, function(){
+            $(this).remove();
+        });
+        edge_r.animate({
+            left: posX + parentWidth
+        }, 600, function(){
+            $(this).remove();
+        });
+    });
     /** Droplet **/
     $(".anim-active-droplet").parent().click(function(e){
-        var posX = e.pageX - $(this).position().left;
-        var posY = e.pageY - $(this).position().top;
+        var posX = e.pageX - $(this).offset().left;
+        var posY = e.pageY - $(this).offset().top;
         var droplet = $("<span class='anim-droplet' style='top:"+posY+";left:"+posX+";'></span>").appendTo($(this));    
-        setTimeout(function() {droplet.remove();}, 1500);
+        setTimeout(function() {droplet.remove();}, 1000);
     });
-    /** Ripple */
-    
 });
